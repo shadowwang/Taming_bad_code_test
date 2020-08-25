cdf92f7e19a960f04aa1b252b79e831bcd254d01
parsonswang <parsonswang@tencent.com>
2020-08-26 00:58:41 +0800

    添加Clock.toLocalTime方法

--- src/com/parsons/refactor/Clock.java
+++ src/com/parsons/refactor/Clock.java
@@ -5,9 +5,13 @@ package com.parsons.refactor;
  */
 public abstract class Clock {
 
-    private final int UTC_OFFSET = 0;
+    private static final int UTC_OFFSET = 0;
 
     private int mLocalTime;
 
     public abstract void setLocalTime(int localTime);
+
+    public static int toLocalTime(int utcZeroTime) {
+        return utcZeroTime + UTC_OFFSET;
+    }
 }
