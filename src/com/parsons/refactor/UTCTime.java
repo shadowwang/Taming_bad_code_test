48ad5d52c15c2a23dbb4c2fe1c20b5dea4f71ee6
parsonswang <parsonswang@tencent.com>
2020-08-26 00:59:14 +0800

    添加UTCTime.setLocalTime方法

--- src/com/parsons/refactor/UTCTime.java
+++ src/com/parsons/refactor/UTCTime.java
@@ -2,10 +2,12 @@ package com.parsons.refactor;
 
 public class UTCTime extends TimeSubject {
 
+    private int mUtcZeroTime;
+
     @Override
     public void notifyAllClocks() {
         for (Clock clock : clocks.values()) {
-            clock.setLocalTime(Clock.toLocalTime(this.utcZeroTime));
+            clock.setLocalTime(Clock.toLocalTime(this.mUtcZeroTime));
         }
     }
 }
