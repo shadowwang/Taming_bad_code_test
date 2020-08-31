faed7587bb7c63b364d42bae78014c0e995fa650
parsonswang <parsonswang@tencent.com>
2020-08-31 23:12:13 +0800

    TDD start

---
+++ src/com/parsons/refactor/tdd/CityClock.java
@@ -0,0 +1,12 @@
+package com.parsons.refactor.tdd;
+
+public class CityClock {
+
+    public CityClock(int offset) {
+
+    }
+
+    public int getTime() {
+        return 1;
+    }
+}
---
+++ src/com/parsons/refactor/tdd/PhoneClock.java
@@ -0,0 +1,13 @@
+package com.parsons.refactor.tdd;
+
+public class PhoneClock {
+    public PhoneClock(int offset) {
+
+    }
+
+    public void setCityClock(CityClock londonClock) {
+    }
+
+    public void setTime(int i) {
+    }
+}
